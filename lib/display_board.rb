6543950c44691b7121e74fd7277a48538475e621
Michael Blandford <mdblandford@gmail.com>
2018-06-14 10:28:17 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,17 +1,7 @@
 def display_board(board)
-  #phrase = "Tic Tac Toe Board"
   puts" #{board[0]} | #{board[1]} | #{board[2]} "
   puts"-----------"
   puts" #{board[3]} | #{board[4]} | #{board[5]} "
   puts"-----------"
   puts" #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-#def display_board(board=[" "," "," "," "," "," "," "," "," "])
-#  phrase = "Tic Tac Toe Board"
-#  puts"   |   |   "
-#  puts"-----------"
-#  puts"   |   |   "
-#  puts"-----------"
-#  puts"   |   |   "
-#end
